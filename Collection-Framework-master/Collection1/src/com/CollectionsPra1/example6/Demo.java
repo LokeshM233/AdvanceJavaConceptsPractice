b70af78d2865c875d6ea1bf0a8774d19115c1969
LokeshM233 <machanilokesh.233@gmail.com>
2025-06-18 12:44:42 +0530

    ChangesInCollectionsPr

---
+++ Collection-Framework-master/Collection1/src/com/CollectionsPra1/example6/Demo.java
@@ -0,0 +1,28 @@
+package com.CollectionsPra1.example6;
+
+import java.util.List;
+
+/**
+ * @author abhishekvermaa10
+ *
+ */
+public class Demo {
+	public static void main(String[] args) {
+		List<String> list = List.of("Java", "Python", "C");
+
+		System.out.println("This list is an unmodifiable list");
+		System.out.println("Contents of list are: " + list);
+
+		System.out.println("Modification of list is not allowed");
+		// list.set(2, "SQL");
+
+		System.out.println("Addition of new element to list is not allowed");
+		// list.add("C#");
+
+		System.out.println("Removal of element from list is not allowed");
+		// list.remove("C#");
+
+		System.out.println("Clear list is not allowed");
+		// list.clear();
+	}
+}
